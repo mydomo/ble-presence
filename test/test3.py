ce75c3955d5f3f3ae5c6d52d3bffc709943756d6
marcobag <marco.bag@gmai.com>
2017-03-30 19:02:44 +0200

    c

--- test/test3.py
+++ test/test3.py
@@ -1,30 +1,28 @@
-import sys
-import bluetooth._bluetooth as bluez
-import ble
 
-def parse_events(sock, loop_count=100):
-    flt = bluez.hci_filter_new()
-    bluez.hci_filter_all_events(flt)
-    bluez.hci_filter_set_ptype(flt, bluez.HCI_EVENT_PKT)
-    sock.setsockopt(bluez.SOL_HCI, bluez.HCI_FILTER, flt)
-    for i in range(loop_count):
-        pkt = sock.recv(255)
-        parsed_packet = ble.hci_le_parse_response_packet(pkt)
-        if "bluetooth_le_subevent_name" in parsed_packet and \
-                parsed_packet["bluetooth_le_subevent_name"] == 'EVT_LE_ADVERTISING_REPORT':
-            for report in parsed_packet["advertising_reports"]:
-                print "Found BLE device:", report['peer_bluetooth_address']
-                for k, v in report.items():
-                    print "\t%s: %s" % (k, v)
+from bluetooth.ble import BeaconService
 
-dev_id = 0
-try:
-    sock = bluez.hci_open_dev(dev_id)
-except:
-    print "Error accessing bluetooth device", dev_id
-    sys.exit(1)
+class Beacon(object):
+    
+    def __init__(self, data, address):
+        self._uuid = data[0]
+        self._major = data[1]
+        self._minor = data[2]
+        self._power = data[3]
+        self._rssi = data[4]
+        self._address = address
+        
+    def __str__(self):
+        ret = "Beacon: address:{ADDR} uuid:{UUID} major:{MAJOR}"\
+                " minor:{MINOR} txpower:{POWER} rssi:{RSSI}"\
+                .format(ADDR=self._address, UUID=self._uuid, MAJOR=self._major,
+                        MINOR=self._minor, POWER=self._power, RSSI=self._rssi)
+        return ret
 
-ble.hci_le_set_scan_parameters(sock)
-ble.hci_le_enable_scan(sock)
-parse_events(sock, 30)
-ble.hci_le_disable_scan(sock)
+service = BeaconService()
+devices = service.scan(2)
+
+for address, data in list(devices.items()):
+    b = Beacon(data, address)
+    print(b)
+
+print("Done.")
