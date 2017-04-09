69565e7437796fc9fcfc243b8955bc8d4bd727c0
marcobag <marco.bag@gmai.com>
2017-04-09 12:31:37 +0200

    fix

--- test5.py
+++ test5.py
@@ -6,14 +6,14 @@ class ScanDelegate(DefaultDelegate):
 
     def handleDiscovery(self, dev, isNewDev, isNewData):
         if isNewDev:
-            print "Discovered device", dev.addr
+            print ("Discovered device", dev.addr)
         elif isNewData:
-            print "Received new data from", dev.addr
+            print ("Received new data from", dev.addr)
 
 scanner = Scanner().withDelegate(ScanDelegate())
 devices = scanner.scan(10.0)
 
 for dev in devices:
-    print "Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi)
+    print ("Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi))
     for (adtype, desc, value) in dev.getScanData():
-        print "  %s = %s" % (desc, value)
+        print ("  %s = %s" % (desc, value))
