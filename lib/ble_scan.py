9732ba298465c8f9020f6d034c3a982dafd9687d
marcobag <marco.bag@gmai.com>
2017-04-09 10:10:49 +0200

    fix

--- lib/ble_scan.py
+++ lib/ble_scan.py
@@ -138,7 +138,7 @@ def parse_events(sock, loop_count=100):
         elif event == bluez.EVT_DISCONN_COMPLETE:
         	i = 0 
         elif event == LE_META_EVENT:
-            subevent, = struct.unpack("B", pkt[3])
+            subevent = struct.unpack("B", pkt[3])
             pkt = pkt[4:]
             if subevent == EVT_LE_CONN_COMPLETE:
             	le_handle_connection_complete(pkt)
