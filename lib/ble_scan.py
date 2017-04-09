f93d6bf41c3db85e69c27ea07451f5af23df0669
marcobag <marco.bag@gmai.com>
2017-04-09 18:57:49 +0200

    ggg

--- lib/ble_scan.py
+++ lib/ble_scan.py
@@ -106,7 +106,7 @@ def parse_events(sock, loop_count=100):
                     # build the return string
                     Adstring = packed_bdaddr_to_string(pkt[report_pkt_offset + 3:report_pkt_offset + 9])
                     Adstring += ",RSSI,"
-                    Adstring += int(time.time())
+                    Adstring += str(int(time.time()))
                     #Prevent duplicates in results
                     if Adstring not in myFullList: myFullList.append(Adstring)
     sock.setsockopt( bluez.SOL_HCI, bluez.HCI_FILTER, old_filter )
