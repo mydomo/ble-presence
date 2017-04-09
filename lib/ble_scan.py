cf3404fbd97111bcbd734b3bd11e133fc45611b1
marcobag <marco.bag@gmai.com>
2017-04-09 08:29:33 +0200

    fix

--- lib/ble_scan.py
+++ lib/ble_scan.py
@@ -1,3 +1,5 @@
+# TUNED AND FIXED FOR PYTHON3
+
 # BLE iBeaconScanner based on https://github.com/adamf/BLE/blob/master/ble-scanner.py
 # JCS 06/07/14
 
@@ -131,11 +133,11 @@ def parse_events(sock, loop_count=100):
         ptype, event, plen = struct.unpack("BBB", pkt[:3])
         #print "--------------" 
         if event == bluez.EVT_INQUIRY_RESULT_WITH_RSSI:
-		i =0
+		i = 0
         elif event == bluez.EVT_NUM_COMP_PKTS:
-                i =0 
+                i = 0 
         elif event == bluez.EVT_DISCONN_COMPLETE:
-                i =0 
+                i = 0 
         elif event == LE_META_EVENT:
             subevent, = struct.unpack("B", pkt[3])
             pkt = pkt[4:]
