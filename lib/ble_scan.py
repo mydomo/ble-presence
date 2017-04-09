557d8b1fd8b02355c457b23ec2d7aee0270e78db
marcobag <marco.bag@gmai.com>
2017-04-09 15:53:06 +0200

    d

--- lib/ble_scan.py
+++ lib/ble_scan.py
@@ -104,7 +104,7 @@ def parse_events(sock, loop_count=100):
                 report_pkt_offset = 0
                 for i in range(0, num_reports):
                     # build the return string
-                    rssi = struct.unpack("b", pkt[1+13*num_reports+i])[0]
+                    rssi = struct.unpack("b", pkt[report_pkt_offset -1])
                     Adstring = packed_bdaddr_to_string(pkt[report_pkt_offset + 3:report_pkt_offset + 9])
                     Adstring += ',' + returnstringpacket(pkt[report_pkt_offset -22: report_pkt_offset - 6])
                     Adstring += ',' + "%i" % returnnumberpacket(pkt[report_pkt_offset -6: report_pkt_offset - 4])
