416f2f2c2db3783e44cfc62d0c9eb7caf7bfdced
marcobag <marco.bag@gmai.com>
2017-04-09 09:16:58 +0200

    fix

--- test.py
+++ test.py
@@ -3,17 +3,29 @@
 
 from lib import ble_scan
 import sys
+import os
+import time
+
 
 import bluetooth._bluetooth as bluez
 
 dev_id = 0
+
+os.system("sudo /etc/init.d/bluetooth restart")
+time.sleep(1)
+os.system("sudo hciconfig hci0 up")
+
 try:
 	sock = bluez.hci_open_dev(dev_id)
 	print ("ble thread started")
 
 except:
 	print ("error accessing bluetooth device…")
-	sys.exit()
+	print ("riavvio in corso...")
+	os.system("sudo /etc/init.d/bluetooth restart")
+	time.sleep(1)
+	os.system("sudo hciconfig hci0 up")
+	#sys.exit()
 
 ble_scan.hci_le_set_scan_parameters(sock)
 ble_scan.hci_enable_le_scan(sock)
