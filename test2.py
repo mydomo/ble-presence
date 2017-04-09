af97963701cebf4a66a706d52496d57da3efd8cc
marcobag <marco.bag@gmai.com>
2017-04-09 12:05:39 +0200

    fix

--- test2.py
+++ test2.py
@@ -93,9 +93,7 @@ def device_inquiry_with_with_rssi(sock):
     done = False
     while not done:
         pkt = sock.recv(255)
-        print (pkt)
-        addr = bluez.ba2str( pkt[1+6*i:1+6*i+6] )
-        print (pkt)
+        printpacket(pkt)
         ptype, event, plen = struct.unpack("BBB", pkt[:3])
         if event == bluez.EVT_INQUIRY_RESULT_WITH_RSSI:
             pkt = pkt[3:]
