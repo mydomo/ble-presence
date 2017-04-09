b3c4344cbd39fbc84c04cd4c812dc8ac8c71fe4e
marcobag <marco.bag@gmai.com>
2017-04-09 15:40:22 +0200

    ddd

--- lib/ble_scan.py
+++ lib/ble_scan.py
@@ -110,7 +110,7 @@ def parse_events(sock, loop_count=100):
                     Adstring += ',' + returnstringpacket(pkt[report_pkt_offset -6: report_pkt_offset - 4])
                     Adstring += ',' + "%i" % returnnumberpacket(pkt[report_pkt_offset -4: report_pkt_offset - 2])
                     Adstring += ',' + returnstringpacket(pkt[report_pkt_offset -4: report_pkt_offset - 2])
-                    Adstring += 'RSSI: ' + str(struct.unpack("b", pkt[report_pkt_offset -1]))
+                    Adstring += 'RSSI: ' + str(struct.unpack("b", pkt[report_pkt_offset -1: report_pkt_offset + 0]))
                #     try:
                 #        Adstring += ',1:' + "%i" % struct.unpack("b", pkt[report_pkt_offset -2:report_pkt_offset -1])
                  #       Adstring += ',2:' + returnstringpacket(pkt[report_pkt_offset -2:report_pkt_offset -1])
