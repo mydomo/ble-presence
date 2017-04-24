afb12e69f9f241d3da1a0ed7364bacfcbc6c7b1c
marcobag <marco.bag@gmai.com>
2017-04-24 18:30:30 +0200

    test

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ soc.connect(("10.50.0.55", 12345))
 
 clients_input = input("What you want to proceed my dear client?\n")  
 soc.send(clients_input.encode("utf8")) # we must encode the string to bytes  
-result_bytes = soc.recv(4096) # the number means how the response can be in bytes  
+result_bytes = soc.recv(1024) # the number means how the response can be in bytes  
 result_string = result_bytes.decode("utf8") # the return will be in bytes, so decode
 
 print("Result from server is {}".format(result_string))  
