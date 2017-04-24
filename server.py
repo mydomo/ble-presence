78b28eb46a6a7c0e4c5dffa19151ee83ba6f1a8f
marcobag <marco.bag@gmai.com>
2017-04-24 16:43:20 +0200

    TEST

--- server.py
+++ server.py
@@ -100,4 +100,24 @@ def start_server():
             traceback.print_exc()
     soc.close()
 
-start_server() 
+### MAIN PROGRAM ###
+import signal
+import time
+
+class GracefulKiller:
+  kill_now = False
+  def __init__(self):
+    signal.signal(signal.SIGINT, self.exit_gracefully)
+    signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+  def exit_gracefully(self,signum, frame):
+    self.kill_now = True
+
+if __name__ == '__main__':
+  killer = GracefulKiller()
+  while True:
+    start_server() 
+    if killer.kill_now:
+      break
+
+  print "End of the program. I was killed gracefully :)"
