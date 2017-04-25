ae606efdb0b2414c8b08e74eea29d08070e99fe5
marcobag <marco.bag@gmai.com>
2017-04-25 20:08:51 +0200

    thread

--- testthread.py
+++ testthread.py
@@ -3,15 +3,15 @@ from threading import Thread
 
 class myClass(Thread):
     def run(self):
-        print 'Starting '
+        print ('Starting ')
         while(True):
-            print '1'
+            print ('1')
 
 class myClassSecond(Thread):
     def run(self):
-        print 'starting second'
+        print ('starting second')
         while(True):
-            print '2'
+            print ('2')
 
 if __name__=='__main__':
     a=myClass()
