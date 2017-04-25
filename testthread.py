c6cf9cb6a86646909d79fb2543c282905572ca58
marcobag <marco.bag@gmai.com>
2017-04-25 20:07:58 +0200

    thread

---
+++ testthread.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from threading import Thread
+
+class myClass(Thread):
+    def run(self):
+        print 'Starting '
+        while(True):
+            print '1'
+
+class myClassSecond(Thread):
+    def run(self):
+        print 'starting second'
+        while(True):
+            print '2'
+
+if __name__=='__main__':
+    a=myClass()
+    b=myClassSecond()
+    a.start()
+    b.start()
