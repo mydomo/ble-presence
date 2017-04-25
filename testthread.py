b8b87c9e2df53452f95662a0887ba25021fc4086
marcobag <marco.bag@gmai.com>
2017-04-25 20:15:10 +0200

    test

--- testthread.py
+++ testthread.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 from threading import Thread
 
-class myClass(Thread):
-    def run(self):
-        print ('Starting ')
-        while(True):
-            print ('1')
+def uno(self):
+    print ('Starting ')
+    while(True):
+        print ('1')
 
-class myClassSecond(Thread):
-    def run(self):
-        print ('starting second')
-        while(True):
-            print ('2')
+def due(self):
+    print ('starting second')
+    while(True):
+        print ('2')
 
 if __name__=='__main__':
-    a=myClass()
-    b=myClassSecond()
-    a.start()
-    b.start()
+    Thread(target=uno).start()
+    Thread(target=due).start()
