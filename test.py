5593998b926e22cd1056181834703c91f8d31a10
marcobag <marco.bag@gmai.com>
2017-04-30 08:12:37 +0200

    Revert "boh"
    This reverts commit af376129c4e157427ae2751eb504565f97eaeecf.

---
+++ test.py
@@ -0,0 +1,45 @@
+# test BLE Scanning software
+# jcs 6/8/2014
+
+from lib import ble_scan
+import sys
+import os
+import time
+
+
+import bluetooth._bluetooth as bluez
+
+dev_id = 0
+
+os.system("sudo /etc/init.d/bluetooth restart")
+time.sleep(1)
+os.system("sudo hciconfig hci0 up")
+
+try:
+	sock = bluez.hci_open_dev(dev_id)
+	print ("ble thread started")
+
+except:
+	print ("error accessing bluetooth device…")
+	print ("riavvio in corso...")
+	os.system("sudo /etc/init.d/bluetooth restart")
+	time.sleep(1)
+	os.system("sudo hciconfig hci0 up")
+	#sys.exit()
+
+ble_scan.hci_le_set_scan_parameters(sock)
+ble_scan.hci_enable_le_scan(sock)
+mybeacon = {}
+while True:
+	returnedList = ble_scan.parse_events(sock, 25)
+	print ("----------")
+	for beacon in returnedList:
+		MAC, RSSI, LASTSEEN = beacon.split(',')
+		#print (MAC)
+		#print (RSSI)
+		#print (LASTSEEN)
+		mybeacon[MAC] = [RSSI,LASTSEEN]
+	print (mybeacon)
+
+
+
