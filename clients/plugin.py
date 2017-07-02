7fee5a97eb4016f472847f9f141730bdf2650a6d
marcobag <marco.bag@gmai.com>
2017-07-02 17:55:01 +0200

    test

--- clients/plugin.py
+++ clients/plugin.py
@@ -1,6 +1,6 @@
 """
 BLE-Presence python plugin for Domoticz
-Author: Marco Baglivo, some parts of the components are fork of other open source projects. Read readme for more informations.
+Author: Marco Baglivo, some parts of the components are fork of other open source projects. Read README for more informations.
 
 Version:    
             0.0.1: pre-alpha
@@ -56,6 +56,34 @@ class BasePlugin:
         
     #BLE-PRESENCE SPECIFIC METHODS
 
+    def BLE_ONLY_devices(self)
+        if not self.error:
+            try:
+                soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+                soc.connect((Parameters["Address"], Parameters["Port"]))
 
+                clients_input = "beacon_data" 
+                soc.send(clients_input.encode()) # we must encode the string to bytes  
+                result_bytes = soc.recv(32768) # the number means how the response can be in bytes  
+                result_string = result_bytes.decode("utf8") # the return will be in bytes, so decode
 
+            except:
+                self.error = True
+                Domoticz.Error("Error connecting to BLE-Server: " + Parameters["Address"] + " on port: " + Parameters["Port"])
+            else:
+                # START THE INPUT CLEANING FOR BEACONING DATA:
+                # REMOVE '[', ']' AND '(' FROM THE RECEIVED STRING
+                if result_string.startswith('[') and result_string.endswith(']'):
+                    result_string = result_string[1:-1].replace("(", "")
+                # RECURSIVE SPLIT THE STRING TO GET THE DATA:
+                items = result_string.split("), ")
+                for item in items:
+                    bucket = item.split("', ['")
+                    BLE_MAC = bucket[0].replace("'", "")
+                    ble_data = bucket[1].split("', '")
+                    BLE_RSSI = ble_data[0]
+                    BLE_TIME = ble_data[1].replace("']", "").replace(")", "")
+                    #TO BE CONTINUED
+        if self.error:
+            self.BatteryNodes = []
 
