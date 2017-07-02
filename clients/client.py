ab36af79e7e93958fa504873611a1cb7c127b15d
marcobag <marco.bag@gmai.com>
2017-07-02 09:14:04 +0200

    test client

--- clients/client.py
+++ clients/client.py
@@ -10,4 +10,10 @@ soc.send(clients_input) # we must encode the string to bytes
 result_bytes = soc.recv(4096) # the number means how the response can be in bytes  
 result_string = result_bytes.decode("utf8") # the return will be in bytes, so decode
 
-print("Result from server is {}".format(result_string))  
+print("Result from server is {}".format(result_string))
+print(" ")
+print("ora la elaboro:")
+print("ELIMINO LE PARENTESI QUADRE DI INIZIO E FINE")
+if result_string.startswith('[') and result_string.endswith(']'):
+    result_string = result_string[1:-1]
+    print (result_string)
