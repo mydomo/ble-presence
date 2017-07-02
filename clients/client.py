e5707e94209079d6113c82f435fa8c34117e5022
marcobag <marco.bag@gmai.com>
2017-07-02 09:19:13 +0200

    py client fix

--- clients/client.py
+++ clients/client.py
@@ -6,8 +6,8 @@ soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 soc.connect(("10.50.0.55", 12345))
 
 clients_input = "beacon_data" 
-soc.send(clients_input) # we must encode the string to bytes  
-result_bytes = soc.recv(4096) # the number means how the response can be in bytes  
+soc.send(clients_input.encode()) # we must encode the string to bytes  
+result_bytes = soc.recv(32768) # the number means how the response can be in bytes  
 result_string = result_bytes.decode("utf8") # the return will be in bytes, so decode
 
 print("Result from server is {}".format(result_string))
