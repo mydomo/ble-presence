6ad91a0035ab14723e3df6a2ca97fd314c965a86
marcobag <marco.bag@gmai.com>
2017-07-02 13:12:45 +0200

    test

--- clients/client.py
+++ clients/client.py
@@ -10,15 +10,13 @@ soc.send(clients_input.encode()) # we must encode the string to bytes
 result_bytes = soc.recv(32768) # the number means how the response can be in bytes  
 result_string = result_bytes.decode("utf8") # the return will be in bytes, so decode
 
-print("Result from server is {}".format(result_string))
-print(" ")
-print("ora la elaboro:")
-print("ELIMINO LE PARENTESI QUADRE DI INIZIO E FINE")
+#print("Result from server is {}".format(result_string))
+
+
+#START THE INPUT CLEANING FOR BEACONING DATA:
+#REMOVE '[' AND ']' FROM THE RECEIVED STRING
 if result_string.startswith('[') and result_string.endswith(']'):
-    result_string = result_string[1:-1]
-    print (result_string)
-    print ("Elimino le tonde di inizio:")
-    result_string = result_string.replace("(", "")
+    result_string = result_string[1:-1].replace("(", "")
     print (result_string)
 print("ORA DIVIDO LA PRIMA VOLTA")
 print(result_string.split("), "))
