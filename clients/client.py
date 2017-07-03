d24243832df1c22e6c349c491c2ed652f3cc13d1
marcobag <marco.bag@gmai.com>
2017-07-03 16:15:42 +0200

    fix

--- clients/client.py
+++ clients/client.py
@@ -11,9 +11,6 @@ soc.send(clients_input.encode()) # we must encode the string to bytes
 result_bytes = soc.recv(32768) # the number means how the response can be in bytes  
 result_string = result_bytes.decode("utf8") # the return will be in bytes, so decode
 
-#print("Result from server is {}".format(result_string))
-
-
 # START THE INPUT CLEANING FOR BEACONING DATA:
 # REMOVE '[', ']' AND '(' FROM THE RECEIVED STRING
 if result_string.startswith('[') and result_string.endswith(']'):
