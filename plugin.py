bfba12cc2eeba4c5d15a8d5dc646ea3867cc1a6a
marcobag <marco.bag@gmai.com>
2017-07-06 11:00:59 +0200

    fix

--- plugin.py
+++ plugin.py
@@ -90,20 +90,17 @@ class BasePlugin:
                     BLE_RSSI = ble_data[0]
                     BLE_TIME = ble_data[1].replace("']", "").replace(")", "")
                     for x in Devices:
-                        Domoticz.Log(str(BLE_MAC.replace(":", ""))
-                        Domoticz.Log(str(Devices[x].DeviceID))
-                        if (str(BLE_MAC.replace(":", "")) != (str(Devices[x].DeviceID)):
+                      Domoticz.Log(str(BLE_MAC.replace(":", "")))
+                      Domoticz.Log(str(Devices[x].DeviceID))
+                        if (str(BLE_MAC.replace(":", ""))) != (str(Devices[x].DeviceID)):
                             UNIT_GENERATED = len(Devices) + 1
                             Domoticz.Device(Name=BLE_MAC, Unit=UNIT_GENERATED, DeviceID=BLE_MAC.replace(":", ""), TypeName="Switch").Create()
-                            Domoticz.Log("New BLE device found and created:" + str(BLE_MAC))
                     #for key, value in Devices.items():
                     #    Domoticz.Log(str(key))
                     #    Domoticz.Log(str(value))
                     #for x in Devices:
                     #    Domoticz.Log("Device:           " + str(x) + " - " + str(Devices[x]))
                     #    Domoticz.Log("External ID:     '" + str(Devices[x].DeviceID) + "'")
-
-                    #TO BE CONTINUED
 global _plugin
 _plugin = BasePlugin()
 
