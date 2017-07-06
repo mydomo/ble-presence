e1128cb84fba9725c63b03dfa4996e9bfab95246
marcobag <marco.bag@gmai.com>
2017-07-06 12:31:39 +0200

    test

--- plugin.py
+++ plugin.py
@@ -62,7 +62,7 @@ class BasePlugin:
             try:
                 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 SERV_ADDR = "10.50.0.55"
-                SERV_PORT = int(Parameters["Port"])
+                SERV_PORT = 12345
                 soc.connect(SERV_ADDR, SERV_PORT)
                 #soc.connect(("10.50.0.55", 12345))
 
