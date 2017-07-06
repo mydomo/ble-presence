a1dcc79f9377ccffe553074f2347da05cf9bf14b
marcobag <marco.bag@gmai.com>
2017-07-06 10:42:28 +0200

    test

--- plugin.py
+++ plugin.py
@@ -89,22 +89,17 @@ class BasePlugin:
                     ble_data = bucket[1].split("', '")
                     BLE_RSSI = ble_data[0]
                     BLE_TIME = ble_data[1].replace("']", "").replace(")", "")
-                    ADD_THIS_DEVICE = False
                     for x in Devices:
                     	if str(BLE_MAC.replace(":", "")) != str(Devices[x].DeviceID):
-                    	    ADD_THIS_DEVICE = True
-
-                    if (ADD_THIS_DEVICE == True) or (len(Devices) == 0):
-                        UNIT_GENERATED = len(Devices) + 1
-                        Domoticz.Device(Name=BLE_MAC, Unit=UNIT_GENERATED, DeviceID=BLE_MAC.replace(":", ""), TypeName="Switch").Create()
-                        Domoticz.Log("New BLE device found and created:" + str(BLE_MAC))
-                        ADD_THIS_DEVICE = False
+                    	    UNIT_GENERATED = len(Devices) + 1
+                            Domoticz.Device(Name=BLE_MAC, Unit=UNIT_GENERATED, DeviceID=BLE_MAC.replace(":", ""), TypeName="Switch").Create()
+                            Domoticz.Log("New BLE device found and created:" + str(BLE_MAC))
                     #for key, value in Devices.items():
                     #    Domoticz.Log(str(key))
                     #    Domoticz.Log(str(value))
-                    for x in Devices:
-                        Domoticz.Log("Device:           " + str(x) + " - " + str(Devices[x]))
-                        Domoticz.Log("External ID:     '" + str(Devices[x].DeviceID) + "'")
+                    #for x in Devices:
+                    #    Domoticz.Log("Device:           " + str(x) + " - " + str(Devices[x]))
+                    #    Domoticz.Log("External ID:     '" + str(Devices[x].DeviceID) + "'")
 
                     #TO BE CONTINUED
 global _plugin
