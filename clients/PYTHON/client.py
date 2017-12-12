0a1c48a9ed1ae409e672cfd2b473a0780b15c010
Giorgio <giorgio.architrave@gmail.com>
2017-12-12 12:09:17 +0100

    catch error

--- clients/PYTHON/client.py
+++ clients/PYTHON/client.py
@@ -4,27 +4,26 @@
 import socket
 
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
-soc.connect(("10.50.0.55", 12345))
+soc.connect(("10.50.0.41", 12345))
 
 clients_input = "beacon_data" 
 soc.send(clients_input.encode()) # we must encode the string to bytes  
-result_bytes = soc.recv(32768) # the number means how the response can be in bytes  
+
+data = b''  # recv() does return bytes
+while True:
+    try:
+        chunk = conn.recv(4096)  # some 2^n number
+        if not chunk:  # chunk == ''
+            break
+
+        data += chunk
+
+    except socket.error:
+        conn.close()
+        break
+
+result_bytes = data # the number means how the response can be in bytes  
 result_string = result_bytes.decode("utf8") # the return will be in bytes, so decode
 
-# START THE INPUT CLEANING FOR BEACONING DATA:
-# REMOVE '[', ']' AND '(' FROM THE RECEIVED STRING
-if result_string.startswith('[') and result_string.endswith(']'):
-    result_string = result_string[1:-1].replace("(", "")
-# RECURSIVE SPLIT THE STRING TO GET THE DATA:
-items = result_string.split("), ")
-for item in items:
-    bucket = item.split("', ['")
-    BLE_MAC = bucket[0].replace("'", "")
-    ble_data = bucket[1].split("', '")
-    BLE_RSSI = ble_data[0]
-    BLE_TIME = ble_data[1].replace("']", "").replace(")", "")
-
-    print (BLE_MAC)
-    print (BLE_RSSI)
-    print (BLE_TIME)
-    print ("----------")
+print (result_string)
+print ("----------")
